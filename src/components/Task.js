46abc064500aaf3afc1a15c00c92130db0886071
Juan Camilo <juancmz98@gmail.com>
2021-07-15 17:06:33 -0500

    Added prop-types to Task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
     <div className={`list-item ${state}`}>
@@ -29,6 +31,22 @@ const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
       </div>
     </div>
   );
+};
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
 };
 
 export default Task;
